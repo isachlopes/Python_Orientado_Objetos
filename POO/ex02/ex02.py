74f640644515fb27fd0883c9f5d0c3414ef784d0
Isac Heres Lopes <isachlopes2@hotmail.com>
2021-11-08 10:57:57 -0300

    Up_05

--- POO/ex02/ex02.py
+++ POO/ex02/ex02.py
@@ -1,4 +1,5 @@
 from datetime import date
+from random import randint
 
 
 class Pessoa:
@@ -16,11 +17,18 @@ class Pessoa:
     def por_ano_nasc(cls, nome, nasc):
         idade = cls.atual - nasc
         return cls(nome, idade)
+    
+    
+    @staticmethod
+    def usuario_id():
+        rand = randint(10000, 99999)
+        return rand
         
 #p1 = Pessoa.por_ano_nasc('Ana', 1999)
 p1 = Pessoa('Luiz', 32)
 print(p1)
 print(p1.idade, p1.nome)
 p1.ano_nascimento()
+print(Pessoa.usuario_id())
         
         
